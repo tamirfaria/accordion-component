88239d89add4a708f250422a86259edb60b967fc
Lucas Tamir Faria <lucas.tamir@rarolabs.com.br>
2021-08-25 14:39:14 -0300

    feat: add Data archive

---
+++ src/components/Data.js
@@ -0,0 +1,18 @@
+export const Data = [
+  {
+    title: "Hegemonia do ambiente político",
+    text: "Pensando mais a longo prazo, a hegemonia do ambiente político estende o alcance e a importância do retorno esperado a longo prazo.",
+  },
+  {
+    title: "Posturas dos órgãos dirigentes",
+    text: "Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a execução dos pontos do programa deve passar por modificações independentemente das posturas dos órgãos dirigentes com relação às suas atribuições.",
+  },
+  {
+    title: "Paradigmas corporativos",
+    text: "As experiências acumuladas demonstram que o consenso sobre a necessidade de qualificação possibilita uma melhor visão global dos paradigmas corporativos.",
+  },
+  {
+    title: "Processo de gerenciamento",
+    text: "Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a execução dos pontos do programa faz parte de um processo de gerenciamento dos relacionamentos verticais entre as hierarquias.",
+  },
+];
